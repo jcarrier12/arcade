29007480cd58e405a4bc1ea8d9966b077550ccc1
Wilber Cortez <60169445+wario158@users.noreply.github.com>
2022-05-04 17:26:06 -0400

    Add files via upload
    Added login html code

--- login.php
+++ login.php
@@ -1,1 +1,11 @@
-<!DOCTYPE html>
+<?php include("top.php"); ?>
+<form action = "login.php">
+    <fieldset>
+        <legend>Login:</legend>
+        <label><strong>Name:</strong></label>
+        <input name = "username"> </br>
+        <input type = "password" name = "password"> </br>
+        <input type = "submit" value = "Login"/>
+    </fieldset>
+</form>
+<?php include("bottom.html"); ?>
