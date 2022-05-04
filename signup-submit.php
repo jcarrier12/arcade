bd56558e30902cb01d1888054c57626b58a793ca
Tyler Najara <104176235+tnajara1917@users.noreply.github.com>
2022-05-04 17:31:22 -0400

    Update signup-submit.php

--- signup-submit.php
+++ signup-submit.php
@@ -1,1 +1,28 @@
-<!DOCTYPE html>
+<?php include("top.php"); ?>
+
+<!--creation of the array with the information submitted -->
+<!--This information will be sent to our database -->
+<?php
+	$email = $_POST["email"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$gender = $_POST["gender"];
+	$favorite_console = $_POST["favorite_console"];
+	
+	$to_write = array(
+		$email,$username,$password,$gender,$favorite_console
+	);
+	$to_write= implode(",",$to_write);
+	
+	//NEED TO ADD REFERENCE TO FIREBASE!!
+	file_put_contents(   ,PHP_EOL.$to_write, FILE_APPEND);
+?>
+	
+	<strong>Thank You!</strong>
+	<br>
+	<br>
+	Welcome to The Arcade, <?php print($username)?>!
+	<br>
+	<br>
+	
+	<?php include("bottom.html"); ?>
