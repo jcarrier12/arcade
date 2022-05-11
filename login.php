1f8771650c4489334ec5efd241aa4a217887b436
Wilber Cortez <Wilbercortez2013@gmail.com>
2022-05-11 00:04:12 -0400

    fixed up the login.php page
    fixed the login.php only missing is adding the functionality and colors

--- login.php
+++ login.php
@@ -1,11 +1,20 @@
 <?php include("top.php"); ?>
-<form action = "login.php">
-    <fieldset>
-        <legend>Login:</legend>
-        <label><strong>Name:</strong></label>
-        <input name = "username"> </br>
-        <input type = "password" name = "password"> </br>
-        <input type = "submit" value = "Login"/>
-    </fieldset>
-</form>
-<?php include("bottom.html"); ?>
+<div class = "login_body">
+    
+    <div class = "login_box">
+        <div id = "login_text">
+            <h1>Login</h1>
+        </div>
+        <div class = "login_form">
+            <form action = "login.php">
+                <fieldset>
+                    <label><strong>Username</strong></label> </br>
+                    <input name = "username" size = "30"> </br>
+                    <label><strong>Password</strong></label> </br>
+                    <input type = "password" name = "password" size = "30"> </br>
+                    <input type = "submit" value = "Login"/>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</div>
