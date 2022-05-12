899d6877473262a5679ba202c4b8646c53515dc3
Tyler Najara <104176235+tnajara1917@users.noreply.github.com>
2022-05-12 14:51:07 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,50 +1,68 @@
 <?php include("top.php"); ?>
 
+<link href = "arcade.css" rel = "stylesheet" />
+
 <h1> The Arcade </h1>
 <h2> Your one place for discussions regarding gaming</h2>
 
 <div class = "home_body">
-
 	<div class = "featured_forums">
+		<div class = "ff_right">
 		<h3><strong>Top 3 Featured Forums</strong></h3>
 		
-		<a href="forum.php">
-		
-		</a>
-	
-		<a href="forum.php">
-		
-		</a>
-	
-		<a href="forum.php">
-		
-		</a>
-	</div>
+			<div class = "ff_left">	
+			
+				<a href="forum.php?=fortnite">
+				<p>Fortnite</p>
+				</a>
 
-	<div class = "popular_post">
-		<h3><strong>Popular Post of the Day</strong><h3>
-		<!--We need a random post here-->
+				<a href="forum.php?=callofduty">
+				<p>Call of Duty</p>
+				</a>
+	
+				<a href="forum.php?=minecraft">
+				<p>Minecraft</p>
+				</a>
+			</div>
+		</div>
 	</div>
+	
+	<hr>
+	
+	<div class = "popular_post">
+		<div class = "pp_right">
+		<h3><strong>Popular Post of the Day</strong><h3>
+			<div class = "pp_left"
+			<!--We need a random post here-->
+			</div>
+		</div>
+	</div>
+	
+	<hr>
 	
 	<div class = "release_dates">
-	<p><strong>15 Upcoming Games</strong></p>
-	<p>Click a Title!</p>
-	<!--Need to add trailer links from youtube-->
-	<p>-<a href = "" title="trailer" >Gotham Knights - October 25, 2022</a></p>
-	<p>-<a href = "" title="trailer" >Hogwart's Legacy - December 2022</a></p>
-	<p>-<a href = "" title="trailer" >God of War Ragnarök - TBD 2022</a></p>
-	<p>-<a href = "" title="trailer" >Redfall - September 2022</a></p>
-	<p>-<a href = "" title="trailer" >Crimson Desert - TBD 2022</a></p>
-	<p>-<a href = "" title="trailer" >Stray - TBD 2022</a></p>
-	<p>-<a href = "" title="trailer" >Avowed - TBD 2022-2023</a></p>
-	<p>-<a href = "" title="trailer" >Wuchang: Fallen Feathers - TBD 2024 </a></p>
-	<p>-<a href = "" title="trailer" >Showa</a></p>
-	<p>-<a href = "" title="trailer" >Lies of P - TBD</a></p>
-	<p>-<a href = "" title="trailer" >Vigilance 2099 - TBD</a></p>
-	<p>-<a href = "" title="trailer" >Final Fantasy XVI - TBD </a></p>
-	<p>-<a href = "" title="trailer" >The Day Before - TBD</a></p>
-	<p>-<a href = "" title="trailer" >The Witcher 4 - TBD</a></p>
-	<p>-<a href = "" title="trailer" >Kingdom Hearts 4 - TBD</a></p>
-	
+		<div class = "rd_right">
+		<p><strong>15 Upcoming Games</strong></p>
+		<p>Click a Title!</p>
+			<div class = "rd_left"
+				<p>-<a href = "" title="trailer" >Gotham Knights - October 25, 2022</a></p>
+				<p>-<a href = "" title="trailer" >Hogwarts Legacy - December 2022</a></p>
+				<p>-<a href = "" title="trailer" >God of War Ragnarök - TBD 2022</a></p>
+				<p>-<a href = "" title="trailer" >Redfall - September 2022</a></p>
+				<p>-<a href = "" title="trailer" >Crimson Desert - TBD 2022</a></p>
+				<p>-<a href = "" title="trailer" >Stray - TBD 2022</a></p>
+				<p>-<a href = "" title="trailer" >Avowed - TBD 2022-2023</a></p>
+				<p>-<a href = "" title="trailer" >Wuchang: Fallen Feathers - TBD 2024 </a></p>
+				<p>-<a href = "" title="trailer" >Showa - TBD</a></p>
+				<p>-<a href = "" title="trailer" >Lies of P - TBD</a></p>
+				<p>-<a href = "" title="trailer" >Vigilance 2099 - TBD</a></p>
+				<p>-<a href = "" title="trailer" >Final Fantasy XVI - TBD </a></p>
+				<p>-<a href = "" title="trailer" >The Day Before - TBD</a></p>
+				<p>-<a href = "" title="trailer" >The Witcher 4 - TBD</a></p>
+				<p>-<a href = "" title="trailer" >Kingdom Hearts 4 - TBD</a></p>
+			</div>
+		</div>
+	</div>
 </div>
+
 <?php include("bottom.html"); ?>
