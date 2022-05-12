b65681726569f1d4b59e57df4379e9967a89b25e
Tyler Najara <104176235+tnajara1917@users.noreply.github.com>
2022-05-11 23:01:33 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,50 @@
+<?php include("top.php"); ?>
+
+<h1> The Arcade </h1>
+<h2> Your one place for discussions regarding gaming</h2>
+
+<div class = "home_body">
+
+	<div class = "featured_forums">
+		<h3><strong>Top 3 Featured Forums</strong></h3>
+		
+		<a href="forum.php">
+		
+		</a>
+	
+		<a href="forum.php">
+		
+		</a>
+	
+		<a href="forum.php">
+		
+		</a>
+	</div>
+
+	<div class = "popular_post">
+		<h3><strong>Popular Post of the Day</strong><h3>
+		<!--We need a random post here-->
+	</div>
+	
+	<div class = "release_dates">
+	<p><strong>15 Upcoming Games</strong></p>
+	<p>Click a Title!</p>
+	
+	<p>-<a href = "" title="trailer" >Gotham Knights - October 25, 2022</a></p>
+	<p>-<a href = "" title="trailer" >Hogwart's Legacy - December 2022</a></p>
+	<p>-<a href = "" title="trailer" >God of War Ragnarök - TBD 2022</a></p>
+	<p>-<a href = "" title="trailer" >Redfall - September 2022</a></p>
+	<p>-<a href = "" title="trailer" >Crimson Desert - TBD 2022</a></p>
+	<p>-<a href = "" title="trailer" >Stray - TBD 2022</a></p>
+	<p>-<a href = "" title="trailer" >Avowed - TBD 2022-2023</a></p>
+	<p>-<a href = "" title="trailer" >Wuchang: Fallen Feathers - TBD 2024 </a></p>
+	<p>-<a href = "" title="trailer" >Showa</a></p>
+	<p>-<a href = "" title="trailer" >Lies of P - TBD</a></p>
+	<p>-<a href = "" title="trailer" >Vigilance 2099 - TBD</a></p>
+	<p>-<a href = "" title="trailer" >Final Fantasy XVI - TBD </a></p>
+	<p>-<a href = "" title="trailer" >The Day Before - TBD</a></p>
+	<p>-<a href = "" title="trailer" >The Witcher 4 - TBD</a></p>
+	<p>-<a href = "" title="trailer" >Kingdom Hearts 4 - TBD</a></p>
+	
+</div>
+<?php include("bottom.html"); ?>
