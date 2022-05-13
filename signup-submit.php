14a55e2ee14210eb34a4cfd1624c8e1a634b6295
jcarrier12 <75959043+jcarrier12@users.noreply.github.com>
2022-05-13 00:30:25 -0400

    Add files via upload

--- signup-submit.php
+++ signup-submit.php
@@ -3,29 +3,32 @@
 <!--creation of the array with the information submitted -->
 <!--This information will be sent to our database -->
 <?php
-	$script = file_get_contents('functions.js');
-	echo "<script>".$script."</script>";
- 
-	$email = $_POST["email"];
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	$gender = $_POST["gender"];
-	$favorite_console = $_POST["favorite_console"];
-	
-/* 	$to_write = array(
-		$email,$username,$password,$gender,$favorite_console
-	);
-	$to_write= implode(",",$to_write); */
-	
-	//NEED TO ADD REFERENCE TO FIREBASE!!
-	writeUserData($email,$username,$password,$gender,$favorite_console);
+	$arr = file("users.txt");
+	$us = $_POST["name"];
+	$em = $POST["email"];
+	for($i = 0; $i < count($arr); $i++){
+        $exp = explode(";",$arr[$i]);
+        if($exp[0]== $us || $exp[1] == $em){
+            header("Location: signup.php");
+			echo "<script>alert('this username or email is already in use');</script>";
+        }
+    }
+
 ?>
 	
 	<strong>Thank You!</strong>
 	<br>
 	<br>
-	Welcome to The Arcade, <?php print($username)?>!
+	Welcome to The Arcade, <?php print($_POST["name"])?>!
 	<br>
 	<br>
-	
+<?php
+	$myfile = fopen( "users/".$_POST["name"].".txt", "w");
+    fwrite($myfile, 1);
+	fclose($myfile);
+	$fp = fopen('users.txt', 'a');
+	fwrite($fp, "\n".$_POST["name"].";".$_POST["email"].";".$_POST["password"].";".$_POST["gender"].";".$_POST["favorite-console"].";");
+	fclose($fp);
+
+?>	
 	<?php include("bottom.html"); ?>
