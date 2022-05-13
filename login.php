d571d37f116b7c7c0181cb7804b8e217d1aecbde
jcarrier12 <75959043+jcarrier12@users.noreply.github.com>
2022-05-12 22:14:02 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
-<?php include("top.php"); ?>
+<?php include("top.php"); 
+$user=$_GET["user"]; 
+$pass=$_GET["pass"];
+?>
 <div class = "login_body">
     
     <div class = "login_box">
@@ -9,12 +12,22 @@
             <form action = "login.php">
                 <fieldset>
                     <label><strong>Username</strong></label> </br>
-                    <input name = "username" size = "30"> </br>
+                    <input type= "text" method= "get" name = "username" size = "30" id="user"> </br>
                     <label><strong>Password</strong></label> </br>
-                    <input type = "password" name = "password" size = "30"> </br>
+                    <input type = "password" method="get" name = "password" size = "30" id="pass"> </br>
                     <input type = "submit" value = "Login"/>
                 </fieldset>
             </form>
+            <?php
+            $arr = file("users.txt");
+            
+            for($i = 0; $i < count(file("users.txt")); $i++){
+                $exp = explode(';','');
+                if($exp[0]== $user && $pass==$exp[2]){
+                    echo "Location: home.php";
+                }
+            }
+            ?>
         </div>
     </div>
 </div>
